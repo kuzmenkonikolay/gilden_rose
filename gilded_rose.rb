7be50447e37a2b45e0c91e8676f0e8166de71de0
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2019-07-01 17:51:59 +0300

    added new item, refactored code and added unit tests

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,50 +5,79 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
       if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
+        item.sell_in -= 1
+
+        case item.name
+          when 'Aged Brie'
+            aged_brie_action(item)
+          when 'Backstage passes to a TAFKAL80ETC concert'
+            backstage_action(item)
+          when 'Conjured Mana Cake'
+            conjured_action(item)
           else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+            if item.sell_in < 0
+              decreases_twice(item)
+            else
+              decreases_in_quality(item)
+            end
         end
       end
     end
+  end
+
+  def increases_in_quality item
+    item.quality += 1 if item.quality < 50
+  end
+
+  def increases_twice item
+    if item.quality < 49
+      item.quality += 2
+    elsif item.quality == 49
+      item.quality += 1
+    end
+  end
+
+  def increase_three_times item
+    if item.quality < 48
+      item.quality += 3
+    elsif item.quality == 48
+      increases_twice(item)
+    else
+      increases_in_quality(item)
+    end
+  end
+
+  def decreases_in_quality item
+    item.quality -= 1 if item.quality > 0 && item.quality != 80
+  end
+
+  def decreases_twice item
+    if item.quality > 1 && item.quality != 80
+      item.quality -= 2
+    elsif item.quality == 1
+      item.quality = 0
+    end
+  end
+
+  def aged_brie_action item
+    increases_in_quality item
+  end
+
+  def backstage_action item
+    if item.sell_in > 5 && item.sell_in <= 10
+      increases_twice(item)
+    elsif item.sell_in > 0 && item.sell_in <= 5
+      increase_three_times(item)
+    elsif item.sell_in <= 0
+      item.quality = 0
+    else
+      increases_in_quality(item)
+    end
+  end
+
+  def conjured_action item
+    decreases_twice(item)
   end
 end
 
--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -2,9 +2,92 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
-  def test_foo
-    items = [Item.new('foo', 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal items[0].name, 'fixme'
+  def setup
+    @items = [
+        Item.new('+5 Dexterity Vest', 10, 20),
+        Item.new('Aged Brie', 2, 0),
+        Item.new('Elixir of the Mongoose', 5, 7),
+        Item.new('Sulfuras, Hand of Ragnaros', 0, 80),
+        Item.new('Sulfuras, Hand of Ragnaros', -1, 80),
+        Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+        Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+        Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+        Item.new('Conjured Mana Cake', 3, 6)
+    ]
+  end
+
+  test 'normal items' do
+    days = 2
+    gilded_rose = GildedRose.new(@items)
+    (0...days).each do |day|
+      gilded_rose.update_quality
+    end
+
+    assert_equal @items[0].quality, 18
+    assert_equal @items[2].quality, 5
+  end
+
+  test 'normal_items_zero_case' do
+    days = 25
+    gilded_rose = GildedRose.new(@items)
+    (0...days).each do |day|
+      gilded_rose.update_quality
+    end
+
+    assert_equal @items[0].quality, 0
+    assert_equal @items[2].quality, 0
+  end
+
+  test 'aged_brie' do
+    days = 3
+    gilded_rose = GildedRose.new(@items)
+    (0...days).each do |day|
+      gilded_rose.update_quality
+    end
+
+    assert_equal @items[1].quality, 3
+  end
+
+  test 'aged_brie_max_case' do
+    days = 100
+    gilded_rose = GildedRose.new(@items)
+    (0...days).each do |day|
+      gilded_rose.update_quality
+    end
+
+    assert_equal @items[1].quality, 50
+  end
+
+  test 'sulfuras_case' do
+    days = 100
+    gilded_rose = GildedRose.new(@items)
+    (0...days).each do |day|
+      gilded_rose.update_quality
+    end
+
+    assert_equal @items[3].quality, 80
+    assert_equal @items[4].quality, 80
+  end
+
+  test 'backstage_case' do
+    days = 5
+    gilded_rose = GildedRose.new(@items)
+    (0...days).each do |day|
+      gilded_rose.update_quality
+    end
+
+    assert_equal @items[5].quality, 26
+    assert_equal @items[6].quality, 50
+    assert_equal @items[7].quality, 0
+  end
+
+  test 'conjured_case' do
+    days = 2
+    gilded_rose = GildedRose.new(@items)
+    (0...days).each do |day|
+      gilded_rose.update_quality
+    end
+
+    assert_equal @items[8].quality, 2
   end
 end
